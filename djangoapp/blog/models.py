a5f428684cdb23e5bc7014425f3a3a1af09c0309
Caio Dantas <caiodantasl.lara@gmail.com>
2023-11-01 13:10:06 -0300

    create: Page models

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -7,16 +7,19 @@ class Tag(models.Model):
         verbose_name = 'Tag'
         verbose_name_plural = 'Tags'
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     slug = models.SlugField(
         unique=True, default=None,
-        null=True, blank=True, max_length=100,
+        null=True, blank=True, max_length=255,
     )
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Category(models.Model):
@@ -24,13 +27,40 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     slug = models.SlugField(
         unique=True, default=None,
-        null=True, blank=True, max_length=100,
+        null=True, blank=True, max_length=255,
     )
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
